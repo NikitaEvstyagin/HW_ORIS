a35d3f15e1b42022d653cc21e12d95feda63386f
nikita2005553 <48449329+nikita2005553@users.noreply.github.com>
2023-12-24 17:19:54 +0300

    Add files via upload

---
+++ HW4/MyHTTPServer_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyHTTPServer;
+using System.Diagnostics.Tracing;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Xml.Schema;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var server = new ServerHandler();
+        await server.Start();
+    }
+}
+
